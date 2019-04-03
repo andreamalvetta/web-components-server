aaa07f6888aed41a0fc6f0b029ec86707639d19e
andreamalvetta <andrea.malvetta@gmail.com>
2019-04-03 15:27:02 +1100

    New update

--- dist/assets/js/precache-manifest.ca31eb66ca1f8fd09a572ba4caffbc4b.js
+++ dist/assets/js/precache-manifest.ca31eb66ca1f8fd09a572ba4caffbc4b.js
@@ -20,16 +20,28 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "assets/js/5.bf0f789d.js"
   },
   {
-    "revision": "3a84627f0f83500b9757dc13b2c6beab",
-    "url": "assets/js/../../manifest.json"
-  },
-  {
     "revision": "ad69e107b1b806cbf5ea607984118163",
     "url": "assets/js/../../favicon.ico"
   },
   {
+    "revision": "3a84627f0f83500b9757dc13b2c6beab",
+    "url": "assets/js/../../manifest.json"
+  },
+  {
+    "revision": "e044a63e034bf10304dad73138b8c74b",
+    "url": "assets/js/../../vendor/webcomponents/webcomponents-loader.js"
+  },
+  {
     "revision": "5958496c24d038cff6035b008df7ea66",
     "url": "assets/js/../../vendor/helpers/babel-helpers-modern.min.js"
+  },
+  {
+    "revision": "c80452ebf1ed83ca0ba794e4476839d3",
+    "url": "assets/js/../../vendor/helpers/babel-helpers.min.js"
+  },
+  {
+    "revision": "5c703e8b191229cbad883fd90fac7732",
+    "url": "assets/js/../../vendor/helpers/regenerator-runtime.min.js"
   },
   {
     "revision": "5a33b1c07d13f231fbf3c1268efe5fa3",
@@ -44,10 +56,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "assets/js/../img/icon-48x48.png"
   },
   {
-    "revision": "5c703e8b191229cbad883fd90fac7732",
-    "url": "assets/js/../../vendor/helpers/regenerator-runtime.min.js"
-  },
-  {
     "revision": "843dae45cc8698138303d886b173174d",
     "url": "assets/js/../img/icon-512x512.png"
   },
@@ -58,14 +66,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "575d4c62b43be848d38a4b4a1aa11b5e",
     "url": "assets/js/../img/icon-96x96.png"
-  },
-  {
-    "revision": "c80452ebf1ed83ca0ba794e4476839d3",
-    "url": "assets/js/../../vendor/helpers/babel-helpers.min.js"
-  },
-  {
-    "revision": "e044a63e034bf10304dad73138b8c74b",
-    "url": "assets/js/../../vendor/webcomponents/webcomponents-loader.js"
   },
   {
     "revision": "04bfdd5b89f793fa817393edacb86b42",
--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-importScripts("assets/js/precache-manifest.b1afece07663966b531dda36b65f9cec.js", "https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox-sw.js");
+importScripts("assets/js/precache-manifest.ca31eb66ca1f8fd09a572ba4caffbc4b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox-sw.js");
 
 /* eslint-disable no-undef */
 
