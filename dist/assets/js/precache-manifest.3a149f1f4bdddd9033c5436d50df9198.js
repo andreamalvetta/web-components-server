4a29e5254686e5f024169ed95261803332756ee5
andreamalvetta <andrea.malvetta@gmail.com>
2019-04-03 15:30:23 +1100

    New update

--- dist/assets/js/precache-manifest.3a149f1f4bdddd9033c5436d50df9198.js
+++ dist/assets/js/precache-manifest.3a149f1f4bdddd9033c5436d50df9198.js
@@ -36,12 +36,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "assets/js/../../vendor/helpers/babel-helpers-modern.min.js"
   },
   {
-    "revision": "c80452ebf1ed83ca0ba794e4476839d3",
-    "url": "assets/js/../../vendor/helpers/babel-helpers.min.js"
-  },
-  {
     "revision": "5c703e8b191229cbad883fd90fac7732",
     "url": "assets/js/../../vendor/helpers/regenerator-runtime.min.js"
+  },
+  {
+    "revision": "c80452ebf1ed83ca0ba794e4476839d3",
+    "url": "assets/js/../../vendor/helpers/babel-helpers.min.js"
   },
   {
     "revision": "5a33b1c07d13f231fbf3c1268efe5fa3",
@@ -60,12 +60,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "assets/js/../img/icon-512x512.png"
   },
   {
-    "revision": "4c890689dccd81dfc8fa95b13a262034",
-    "url": "assets/js/../img/icon-72x72.png"
-  },
-  {
     "revision": "575d4c62b43be848d38a4b4a1aa11b5e",
     "url": "assets/js/../img/icon-96x96.png"
+  },
+  {
+    "revision": "4c890689dccd81dfc8fa95b13a262034",
+    "url": "assets/js/../img/icon-72x72.png"
   },
   {
     "revision": "04bfdd5b89f793fa817393edacb86b42",
--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-importScripts("assets/js/precache-manifest.ca31eb66ca1f8fd09a572ba4caffbc4b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox-sw.js");
+importScripts("assets/js/precache-manifest.3a149f1f4bdddd9033c5436d50df9198.js", "https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox-sw.js");
 
 /* eslint-disable no-undef */
 
