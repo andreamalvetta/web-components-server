8311d0b4aa085e922514ab5cda89e88ddb7b55ff
andreamalvetta <andrea.malvetta@gmail.com>
2019-05-31 14:52:37 +1000

    Update demo

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.c3e9951427d9beea29a953a2b575413c.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.7a97b575aa5c7e668cab8c565915ce80.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable no-undef */
 
